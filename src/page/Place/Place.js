94c087cb9094deed264e8bc8ecbdefba419618f6
arifusinbad <ariful.islam.sinbad@gmail.com>
2022-11-09 01:56:07 +0600

    commit 9

---
+++ src/page/Place/Place.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Place = () => {
+ return (
+  <div>
+  <div>
+  <h1 className='text-5xl font-bold mt-16'>Traveling Place</h1>
+  <p className='w-1/2 mx-auto mt-5'>Islamic tourism is not only related to religious values but also must be in line with every way of life that is not contrary to Islam. Not many companies and agencies organize halal tourism packages because they think it is not a necessity and a profit.</p>
+  </div>
+  
+   
+   
+  </div>
+ );
+};
+
+export default Place;
