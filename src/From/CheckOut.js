32418dd4d256e74595f8cddf5674dce47e9bb1b1
arifusinbad <ariful.islam.sinbad@gmail.com>
2022-11-09 12:54:38 +0600

    commit 15

--- src/From/CheckOut.js
+++ src/From/CheckOut.js
@@ -4,17 +4,17 @@ import { AuthContext } from '../Authprovider/AuthProvider';
 const CheckOut = () => {
  const{user}=useContext(AuthContext)
  return (
-  <div className='my-20'>
-   <h1>Booking Title : {}</h1>
-   <p>Booking Charge : {}</p>
+  <div className='my-20 w-10/12 mx-auto'>
+   <h1 className='text-2xl font-bold mb-4'>Booking Title : {}</h1>
+   <p className='font-bold'>Booking Charge : ${}</p>
    <form >
-   <div className='grid grid-cols-1 lg:grid-cols-2 gap-7 w-10/12 mx-auto'>
-   <input type="text" placeholder="Type here" className="input input-bordered input-warning w-full " />
-   <input type="text" placeholder="Type here" className="input input-bordered input-warning w-full " />
-   <input type="text" placeholder="Type here" className="input input-bordered input-warning w-full " />
+   <div className='grid grid-cols-1 lg:grid-cols-2 gap-7 mt-7'>
+   <input type="text"  placeholder="Enter Your Full Name" className="input input-bordered input-warning w-full " />
+   <input type="text" placeholder="Cary Catagory" className="input input-bordered input-warning w-full " />
+   <input type="text" placeholder="Phone Number" className="input input-bordered input-warning w-full " />
    <input type="text" defaultValue={user?.email} placeholder="Type here" className="input input-bordered input-warning w-full " />
-   <textarea className="textarea textarea-info" placeholder="Bio"></textarea>
-   <textarea className="textarea textarea-info" placeholder="Bio"></textarea>
+   <textarea className="textarea textarea-info" placeholder="Address"></textarea>
+   <textarea className="textarea textarea-info" placeholder="Description"></textarea>
    <button className="btn btn-wide">PLACE YOUR Booking</button>
 
    </div>
--- src/page/Place/Place.js
+++ src/page/Place/Place.js
@@ -4,7 +4,7 @@ const Place = () => {
  return (
   <div>
   <div>
-  <h1 className='text-5xl font-bold mt-16'>Traveling Place</h1>
+  <h1 className='text-5xl font-bold mt-16 text-center'>Traveling Place</h1>
   <p className='w-1/2 mx-auto mt-5'>Islamic tourism is not only related to religious values but also must be in line with every way of life that is not contrary to Islam. Not many companies and agencies organize halal tourism packages because they think it is not a necessity and a profit.</p>
   </div>
   
